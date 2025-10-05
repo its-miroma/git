a827a0f471f9df177f863fc56056c0ae1039d11d
René Scharfe <l.s.r@web.de>
2025-10-05 17:55:24 +0200

    add-patch: document that option J rolls over
    The variable "permitted" is only not reset after moving to a different
    hunk, so it only accumulates permission and doesn't necessarily reflect
    those of the current hunk.  This may be a bug, but is actually useful
    with the option J, which can be used at the last hunk to roll over to
    the first hunk.  Make this particular behavior official.
    
    Suggested-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: René Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- add-patch.c
+++ add-patch.c
@@ -1398,7 +1398,7 @@ static size_t display_hunks(struct add_p_state *s,
 
 static const char help_patch_remainder[] =
 N_("j - go to the next undecided hunk\n"
-   "J - go to the next hunk\n"
+   "J - go to the next hunk, roll over at the bottom\n"
    "k - go to the previous undecided hunk\n"
    "K - go to the previous hunk\n"
    "g - select a hunk to go to\n"
@@ -1493,7 +1493,7 @@ static int patch_update_file(struct add_p_state *s,
 				permitted |= ALLOW_GOTO_NEXT_UNDECIDED_HUNK;
 				strbuf_addstr(&s->buf, ",j");
 			}
-			if (hunk_index + 1 < file_diff->hunk_nr) {
+			if (file_diff->hunk_nr > 1) {
 				permitted |= ALLOW_GOTO_NEXT_HUNK;
 				strbuf_addstr(&s->buf, ",J");
 			}
